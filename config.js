e746910ea14f40e979e2f387b90d2fe123784cb4
YardenPerez <44260060+YardenPerez@users.noreply.github.com>
2020-11-12 02:54:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.token = "8ba8034e957d50a820b3bb8b4b60e85714e5d869"; //
+config.trading_api_host = 'api-demo.fxcm.com';
+config.trading_api_port = 443;
+config.trading_api_proto = 'https'; // http or https
+
+module.exports = config;
